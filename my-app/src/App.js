905657cba4c960e4d3c8116751853296730950d5
angyumi <ewhgyu@ewhain.net>
2022-03-15 16:42:29 +0900

    Add components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Template from "./components/Template";
 
 // use state, use effect 
 // todo list 만들기 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Template>헬로 월드</Template>
     </div>
   );
 }
---
+++ my-app/src/components/Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+
+
+const Template = () => {
+    return (
+        <div>
+        <div>해야할 작업</div>
+        <div>할일 목록</div>
+        </div>
+    )
+}
